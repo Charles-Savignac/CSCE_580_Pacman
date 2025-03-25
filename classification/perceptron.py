eee400a263a8f0e4a355e5a1dc4a1ab3f8487b49
Charles Savignac <savc1202@usherbrooke.ca>
2025-03-25 10:23:35 -0400

    Question 1

--- classification/perceptron.py
+++ classification/perceptron.py
@@ -50,12 +50,23 @@ class PerceptronClassifier:
         self.features = trainingData[0].keys() # could be useful later
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-
+        weights = self.weights.copy()
         for iteration in range(self.max_iterations):
             print("Starting iteration ", iteration, "...")
             for i in range(len(trainingData)):
+
                 "*** YOUR CODE HERE ***"
-                util.raiseNotDefined()
+
+                datum = trainingData[i]
+                yPrime = self.classify([datum])[0]
+                y = trainingLabels[i]
+
+                if y is not yPrime:
+                    self.weights[y] += datum
+                    self.weights[yPrime] -=  datum
+
+
+
 
     def classify(self, data ):
         """
